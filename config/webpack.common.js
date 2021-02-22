d0319ac5d3c6a122194cfe9e4886a133492b7e45
hezijian6338 <627222344@qq.com>
2021-02-22 20:22:38 +0800

    调整静态文件的基础路径

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -4,6 +4,11 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const ProgressBarPlugin = require('progress-bar-webpack-plugin')
 
 module.exports = {
+  performance: {
+    hints: 'error', 
+    maxAssetSize: 300000, // 整数类型（以字节为单位）
+    maxEntrypointSize: 500000 // 整数类型（以字节为单位）
+  },
   resolve: {
     extensions: ['.js', '.vue', '.scss', '.css'], // 后缀名自动补全
     alias: {
@@ -22,11 +27,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test:/\.js$/, //用正则匹配文件，用require或者import引入的都会匹配到
-        loader:"babel-loader", //加载器名，就是上一步安装的loader
-        exclude:/node_modules/ //排除node_modules目录，我们不加载node模块中的js哦~
-    },
-      {
         test: /\.css$/,
         use: ['style-loader', 'vue-style-loader', 'css-loader']
       },
@@ -37,9 +37,9 @@ module.exports = {
             loader: 'url-loader',
             options: {
               limit: 244000, // 50kb以内转换成base64
-              publicPath: "/", // 外部引入时的路径前缀
-              outputPath: "assets/", // 导出的指定路径
-              name: '[name].[hash:8].[ext]' // 文件名
+              publicPath: "/img", // 外部引入时的路径前缀
+              // outputPath: "assets/", // 导出的指定路径
+              name: 'img/[name].[hash:8].[ext]' // 文件名
             }
           }
         ]
