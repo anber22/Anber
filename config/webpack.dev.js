9329f821080b2ad71466ccfbfecbdaebdeb837b3
anber <1274681881@qq.com>
2021-03-09 20:53:32 +0800

    WL-975 项目整合

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -15,8 +15,8 @@ module.exports = merge(base, {
     // },
     historyApiFallback: true,
     compress: true, // 启用压缩,
-    host: "0.0.0.0",
-    port: 1209, // 端口
+    // host: "0.0.0.0",
+    port: 1211, // 端口
     open: true, // 自动打开浏览器
     hot: true,
     proxy: {
