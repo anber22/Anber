ae3773ea4f457ab1d96afdcb4db9549a90d61a10
phs <1499115834@qq.com>
2021-03-11 19:38:37 +0800

    优化zoom缩放影响echarts

--- src/views/App.vue
+++ src/views/App.vue
@@ -23,7 +23,7 @@ export default {
 }
 @media (min-width: 300px) and (max-width: 320px) {
   #app{
-    zoom: 0.8533;
+    zoom: 0.85333333;
   }
 }
 @media (min-width: 321px) and (max-width: 350px) {
@@ -31,12 +31,12 @@ export default {
     zoom: 0.93333;
   }
 }
-@media (min-width: 351px) and (max-width: 370px) {
+@media (min-width: 351px) and (max-width: 374px) {
   #app{
     zoom: 0.989333;
   }
 }
-@media (min-width: 371px) and (max-width: 379px) {
+@media (min-width: 375px) and (max-width: 379px) {
   #app{
     zoom: 1;
   }
@@ -58,14 +58,24 @@ export default {
   }
 
 }
-@media (min-width: 421px) and (max-width: 500px) {
+@media (min-width: 421px) and (max-width: 440px) {
   #app{
-    zoom: 1.333333;
+     zoom: 1.1733333;
   }
 }
-@media (min-width: 500px) and (max-width: 540px) {
+@media (min-width: 441px) and (max-width: 460px) {
   #app{
-    zoom: 1.44;
+    zoom: 1.2266666;
+  }
+}
+@media (min-width: 461px) and (max-width: 480px) {
+  #app{
+    zoom: 1.28;
+  }
+}
+@media (min-width: 481px) and (max-width: 500px) {
+  #app{
+    zoom: 1.33333333;
   }
 }
 .AnalysisPie-echarts-tooltip{
